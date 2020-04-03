6961b23f878692e1bb159d6e9b8a3063a39039cf
Nicholas Monath <nmonath@users.noreply.github.com>
2020-04-02 22:38:27 -0400

    Update install_mvn.sh

--- bin/install_mvn.sh
+++ bin/install_mvn.sh
@@ -4,6 +4,6 @@ set -xu
 
 mkdir $GHHC_ROOT/dep
 pushd $GHHC_ROOT/dep
-wget http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
-tar -xvf apache-maven-3.6.1-bin.tar.gz
+wget http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar -xvf apache-maven-3.6.3-bin.tar.gz
 popd
