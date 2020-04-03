c78099d4d576831e414fa1c26ceec8622572dfaa
Nicholas Monath <nmonath@users.noreply.github.com>
2020-04-02 22:38:41 -0400

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,4 +3,4 @@
 export GHHC_ROOT=`pwd`
 
 export PYTHONPATH=$GHHC_ROOT/src/python:$PYTHONPATH
-export PATH=$GHHC_ROOT/dep/apache-maven-3.6.1/bin:$PATH
+export PATH=$GHHC_ROOT/dep/apache-maven-3.6.3/bin:$PATH
